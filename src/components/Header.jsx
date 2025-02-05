5f36f8608f4f831c356ab11ba725c9b9a8e8d3ba
Oleksandra <ssasha.yurchenko@gmail.com>
2025-02-05 18:25:27 +0200

    Update package.json for Netlify deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return (
         <header>
-        <img src="./src/assets/globe.png" alt="Globe logo"/>
+        <img src="../../assets/globe.png" alt="Globe logo"/>
         <span>my travel journal.</span>
         </header>
     )
